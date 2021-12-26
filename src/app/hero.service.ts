7daf3c56cb0c5d1506617ad90711d23f0dc5e722
yuki <yuki.onishi1129@gmail.com>
2021-12-26 23:32:22 +0900

    add: message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -7,11 +8,12 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     // 非同期処理でデータ取得を実行する
     const heroes = of(HEROES);
+    this.messageService.add(`HeroService: fetched heroes`);
     return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -18,7 +18,10 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
   // service層をDIでインスタンス化
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   // ライフサイクルフック
   // コンポーネント作成直後に処理を実行
@@ -32,6 +35,7 @@ export class HeroesComponent implements OnInit {
    */
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   /**
